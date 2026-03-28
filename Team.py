32b7c77657726b036e7c8d69524664a2562b6a59
Manuel Jermann <manueljermann02@gmail.com>
2026-03-28 15:57:58 +0100

    Verbesserung Klasse Team

--- Team.py
+++ Team.py
@@ -2,11 +2,10 @@ from datetime import date
 
 
 class Team:
-    def __init__(self, name: str, stadt: str, gruendungsjahr: date.year):
+    def __init__(self, name: str, stadt: str, gruendungsjahr: date):
         self.name = name
         self.stadt = stadt
         self.gruendungsjahr = gruendungsjahr
 
-
-def __str__(self):
-    return f"{self.name} ({self.stadt}), gegründet im Jahr: {self.gruendungsjahr}"
+    def __str__(self):
+        return f"{self.name} ({self.stadt}), gegründet am: {self.gruendungsjahr}"
