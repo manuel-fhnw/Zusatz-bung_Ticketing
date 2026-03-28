91b03f05b8a5fd526e1b5842c4c09eaee7d2d349
Manuel Jermann <manueljermann02@gmail.com>
2026-03-28 15:12:32 +0100

    Verbesserung Kundenklasse

--- Kunden.py
+++ Kunden.py
@@ -1,8 +1,21 @@
 class Kunde:
     def __init__(self, vorname, nachname, mail):
+        if "@" not in mail:
+            raise ValueError("Ungültige E-Mail-Adresse")
+
         self.vorname = vorname
         self.nachname = nachname
         self.mail = mail
 
+    @property
+    def name(self):
+        return f"{self.vorname} {self.nachname}"
+
     def __str__(self):
-        return f"{self.vorname} {self.nachname} ({self.mail})"
+        return f"{self.name} ({self.mail})"
+
+    def __eq__(self, other):
+        return isinstance(other, Kunde) and self.mail == other.mail
+
+    def __hash__(self):
+        return hash(self.mail)
